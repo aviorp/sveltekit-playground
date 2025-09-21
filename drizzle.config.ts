6d8d65ef18874a6d32636882345dc69555ba18f6
Avior P <avior.peretz@navina.ai>
2025-09-22 02:59:10 +0300

    refactor database structure; move schemas to new directory and update imports

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ if (!process.env.VITE_DATABASE_URL)
   throw new Error("VITE_DATABASE_URL is not set");
 
 export default defineConfig({
-  schema: "./src/lib/server/db/schemas/schema.ts",
+  schema: "./src/db/schemas/schema.ts",
   dialect: "postgresql",
   dbCredentials: { url: process.env.VITE_DATABASE_URL },
   verbose: true,
